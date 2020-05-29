ab1b1257f0cee0ddf727aca550022c135c286693
Shi Kai <shi.kai@outlook.com>
2020-05-29 16:02:09 -0400

    set up the server

---
+++ src/build/precache-manifest.f6669885d827e672f801561ec9eee7f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53b63a939b633921eb4151f83b056c02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98a2421e89ef1f9c4e52",
+    "url": "/static/css/main.f6ef2307.chunk.css"
+  },
+  {
+    "revision": "4ead43ef60593c64097d",
+    "url": "/static/js/2.62626ff1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.62626ff1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98a2421e89ef1f9c4e52",
+    "url": "/static/js/main.98849451.chunk.js"
+  },
+  {
+    "revision": "053c8e34507918769709",
+    "url": "/static/js/runtime-main.aecaeffa.js"
+  }
+]);
